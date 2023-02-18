279de654fd885b0481e919e9477bcb8fa0bfaa35
JFMajer <jakubmajer@live.com>
2023-02-18 15:30:40 +0100

    deploy to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,14 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    const bucket = core.getInput('bucket-name', { required: true, trimWhitespace: true });
+    const region = core.getInput('region', { required: true, trimWhitespace: true });
+    const distFolder = core.getInput('dist-folder', { required: true, trimWhitespace: true });
+
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${region}`);
+
     core.notice('Hello from my custom JS Action!');
 }
 
